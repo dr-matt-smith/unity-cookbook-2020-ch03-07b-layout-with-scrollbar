8db0dbb690296a781a485d7e88a3fef8bb478d59
ShaunFerns <shaun.ferns@tudublin.ie>
2021-05-19 13:24:33 +0100

    updated for Unity 2021

--- Assets/_Scripts/PlayerInventoryDisplay.cs
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -1,40 +1,39 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(PlayerInventory))]
-public class PlayerInventoryDisplay : MonoBehaviour
+public class PlayerInventoryDisplay : MonoBehaviour 
 {
-	private int numInventorySlots;
-	private PickupUI[] slots;
+	const int NUM_INVENTORY_SLOTS = 10;
+
+	// array of references to slot panel GameObjects
+	private PickupUI[] slots = new PickupUI[NUM_INVENTORY_SLOTS];
 
 	public GameObject panelSlotGrid;
 	public GameObject starSlotPrefab;
 
 	void Awake()
 	{
-		GameObject[] gameObjectsTaggedStar = GameObject.FindGameObjectsWithTag("Star");
-		numInventorySlots = gameObjectsTaggedStar.Length;
+		float width = 50 + (NUM_INVENTORY_SLOTS * 50);
 
-		slots = new PickupUI[numInventorySlots];
-		float width = 50 + (numInventorySlots * 50);
 		panelSlotGrid.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
 
-		for (int i = 0; i < numInventorySlots; i++)
-		{
+		for(int i=0; i < NUM_INVENTORY_SLOTS; i++){
 			GameObject starSlotGO = (GameObject)
 				Instantiate(starSlotPrefab);
 			starSlotGO.transform.SetParent(panelSlotGrid.transform);
-			starSlotGO.transform.localScale = new Vector3(1, 1, 1);
+			starSlotGO.transform.localScale = new Vector3(1,1,1);
 			slots[i] = starSlotGO.GetComponent<PickupUI>();
 		}
 	}
 
-
+	
 	public void OnChangeStarTotal(int starTotal)
 	{
-		for (int i = 0; i < numInventorySlots; i++)
-		{
+		for(int i = 0; i < NUM_INVENTORY_SLOTS; i++){
 			PickupUI slot = slots[i];
-			if (i < starTotal)
+			if(i < starTotal)
 				slot.DisplayColorIcon();
 			else
 				slot.DisplayGreyIcon();
